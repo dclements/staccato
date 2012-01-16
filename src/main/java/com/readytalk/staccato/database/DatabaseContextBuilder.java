6475b0cf2d1f7c3bb3aacbabf19117065e284a21
David H. Clements <dclemen@mac.com>
2012-01-16 10:34:36 -0700

    Using a more complete builder pattern for DatabaseContextBuilder.

--- src/main/java/com/readytalk/staccato/database/DatabaseContextBuilder.java
+++ src/main/java/com/readytalk/staccato/database/DatabaseContextBuilder.java
@@ -3,22 +3,99 @@ package com.readytalk.staccato.database;
 import java.net.URI;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.Builder;
 
-public class DatabaseContextBuilder {
+public class DatabaseContextBuilder implements Builder<DatabaseContext> {
 
 	private final DatabaseContext ctx = new DatabaseContext();
-
+	
 	/**
-	 * Returns the database context
+	 * Returns the database context.
 	 *
 	 * @return {@link com.readytalk.staccato.database.DatabaseContext}
 	 */
 	public DatabaseContext build() {
+		
+		this.fullyQualifiedJdbcUriStr(ctx.getBaseJdbcUri(), ctx.getDbName());
+		
+		if(StringUtils.isEmpty(ctx.getSuperUser())) {
+			ctx.setSuperUser(ctx.getDatabaseType().getRoot());
+		}
+		
+		if (StringUtils.isEmpty(ctx.getRootDbName())) {
+			ctx.setRootDbName(ctx.getDatabaseType().getRoot());
+		} 
+		
 		return ctx;
+	}
+	
+	public DatabaseContextBuilder jdbcUri(String jdbcUri) {
+		ctx.setBaseJdbcUri(URI.create(jdbcUri));
+		
+		return this;
+	}
+	
+	private DatabaseContextBuilder fullyQualifiedJdbcUriStr(URI jdbcUri, String dbName) {
+		String fullyQualifiedJdbcUriStr = jdbcUri.toString();
+		
+		if (fullyQualifiedJdbcUriStr.endsWith("/")) {
+			fullyQualifiedJdbcUriStr += dbName;
+		} else {
+			fullyQualifiedJdbcUriStr += "/" + dbName;
+		}
+		
+		final URI fullyQualifiedJdbcUri = URI.create(fullyQualifiedJdbcUriStr);
+		
+		ctx.setFullyQualifiedJdbcUri(fullyQualifiedJdbcUri);
+		
+		DatabaseType databaseType = DatabaseType.getTypeFromJDBCUri(fullyQualifiedJdbcUri);
+		ctx.setDatabaseType(databaseType);
+		
+		return this;
+	}
+	
+	public DatabaseContextBuilder dbName(String dbName) {
+		ctx.setDbName(dbName);
+		
+		return this;
+	}
+	
+	public DatabaseContextBuilder username(String username) {
+		ctx.setUsername(username);
+		
+		return this;
+	}
+	
+	public DatabaseContextBuilder password(String password) {
+		ctx.setPassword(password);
+		
+		return this;
+	}
+	
+	public DatabaseContextBuilder superUser(String superUser) {
+		ctx.setSuperUser(superUser);
+		
+		return this;
+	}
+	
+	public DatabaseContextBuilder superUserPwd(String superUserPwd) {
+		if (StringUtils.isEmpty(superUserPwd)) {
+			ctx.setSuperUserPwd("");
+		} else {
+			ctx.setSuperUserPwd(superUserPwd);
+		}
+		
+		return this;
+	}
+	
+	public DatabaseContextBuilder rootDbName(String rootDbName) {
+		ctx.setRootDbName(rootDbName);
+		
+		return this;
 	}
 
 	/**
-	 * Sets the base jdbc context
+	 * Convenience method for setting the base JDBC context.
 	 *
 	 * @param jdbcUri the base jdbc uri
 	 * @param dbName the database name
@@ -31,43 +108,6 @@ public class DatabaseContextBuilder {
 
 	public DatabaseContextBuilder setContext(String jdbcUri, String dbName, String username, String password, String superUser, String superUserPwd, String rootDbName) {
 
-		String fullyQualifiedJdbcUriStr = jdbcUri.toString();
-
-		if (fullyQualifiedJdbcUriStr.endsWith("/")) {
-			fullyQualifiedJdbcUriStr += dbName;
-		} else {
-			fullyQualifiedJdbcUriStr += "/" + dbName;
-		}
-
-		URI fullyQualifiedJdbcUri = URI.create(fullyQualifiedJdbcUriStr);
-
-		ctx.setBaseJdbcUri(URI.create(jdbcUri));
-		ctx.setFullyQualifiedJdbcUri(fullyQualifiedJdbcUri);
-		ctx.setDbName(dbName);
-		ctx.setUsername(username);
-		ctx.setPassword(password);
-
-		DatabaseType databaseType = DatabaseType.getTypeFromJDBCUri(fullyQualifiedJdbcUri);
-		ctx.setDatabaseType(databaseType);
-
-		if (StringUtils.isEmpty(rootDbName)) {
-			ctx.setRootDbName(databaseType.getRoot());
-		} else {
-			ctx.setRootDbName(rootDbName);
-		}
-
-		if (StringUtils.isEmpty(superUser)) {
-			ctx.setSuperUser(databaseType.getRoot());
-		} else {
-			ctx.setSuperUser(superUser);
-		}
-
-		if (StringUtils.isEmpty(superUserPwd)) {
-			ctx.setSuperUserPwd("");
-		} else {
-			ctx.setSuperUserPwd(superUserPwd);
-		}
-
-		return this;
+		return this.jdbcUri(jdbcUri).dbName(dbName).username(username).password(password).superUser(superUser).superUserPwd(superUserPwd).rootDbName(rootDbName);
 	}
 }
