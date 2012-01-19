82a3b8e90d69b5a71ce1834f2721d9c689fc6886
David H. Clements <dclemen@mac.com>
2012-01-19 12:45:16 -0700

    Adding additional unit tests.
    - Brought GroovyScript under test.
    - More work with GroovyMigrationService and GroovyScriptService.

--- src/test/true_unit/com/readytalk/staccato/database/migration/GroovyMigrationServiceTest.java
+++ src/test/true_unit/com/readytalk/staccato/database/migration/GroovyMigrationServiceTest.java
@@ -2,13 +2,15 @@ package com.readytalk.staccato.database.migration;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.lang.annotation.IncompleteAnnotationException;
-import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
---
+++ src/test/true_unit/com/readytalk/staccato/database/migration/script/groovy/JUGroovyScriptTest.java
@@ -0,0 +1,135 @@
+package com.readytalk.staccato.database.migration.script.groovy;
+
+import static org.junit.Assert.*;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.readytalk.staccato.database.DatabaseType;
+
+public class JUGroovyScriptTest {
+	private GroovyScript script1;
+	private GroovyScript script2;
+	private final DateTimeFormatter parser = DateTimeFormat.forPattern("DDD-YYYYY HH:mm:ss");
+	
+	@Before
+	public void setUp() throws Exception {
+		script1 = new GroovyScript();
+		script2 = new GroovyScript();
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+	
+	@Test
+	public void testCompareToDifferent() {
+		script1.setDatabaseType(DatabaseType.HSQLDB);
+		script2.setDatabaseType(DatabaseType.POSTGRESQL);
+		
+		assertEquals(1, script1.compareTo(script2));
+	}
+	
+	@Test
+	public void testCompareToDates() {
+		script1.setDatabaseType(DatabaseType.HSQLDB);
+		script2.setDatabaseType(DatabaseType.HSQLDB);
+		
+		script1.setScriptDate(parser.parseDateTime("001-2012 13:00:00"));
+		script2.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		
+		//FIXME: Is this really correct ordering?
+		assertTrue(script1.compareTo(script2) > 0);
+	}
+	
+	@Test
+	public void testCompareToDatesReversed() {
+		script1.setDatabaseType(DatabaseType.HSQLDB);
+		script2.setDatabaseType(DatabaseType.HSQLDB);
+		
+		script1.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		script2.setScriptDate(parser.parseDateTime("001-2012 13:00:00"));
+		
+		assertTrue(script1.compareTo(script2) < 0);
+	}
+	
+	@Test
+	public void testCompareToDatesSame() {
+		script1.setDatabaseType(DatabaseType.HSQLDB);
+		script2.setDatabaseType(DatabaseType.HSQLDB);
+		
+		script1.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		script2.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		
+		assertEquals(0, script1.compareTo(script2));
+	}
+
+	
+	@Test
+	public void testEqualsIdentity() {
+		assertTrue(script1.equals(script1));
+	}
+	
+	@Test
+	public void testEqualsObject() {
+		assertFalse(script1.equals(new Object()));
+	}
+	
+	@Test
+	public void testEqualsNull() {
+		assertFalse(script1.equals(null));
+	}
+	
+	@Test
+	public void testEqualsTrue() {
+		script1.setDatabaseType(DatabaseType.HSQLDB);
+		script2.setDatabaseType(DatabaseType.HSQLDB);
+		
+		script1.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		script2.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		
+		assertTrue(script1.equals(script2));
+	}
+	
+	@Test
+	public void testEqualsFalse() {
+		script1.setDatabaseType(DatabaseType.HSQLDB);
+		script2.setDatabaseType(DatabaseType.POSTGRESQL);
+		
+		assertFalse(script1.equals(script2));
+	}
+	
+	@Test
+	public void testHashCodeEquals() {
+		script1.setDatabaseType(DatabaseType.HSQLDB);
+		script2.setDatabaseType(DatabaseType.HSQLDB);
+		
+		script1.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		script2.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		
+		assertEquals(script1.hashCode(), script2.hashCode());
+	}
+	
+	@Test
+	public void testHashCodeDifferent() {
+		script1.setDatabaseType(DatabaseType.HSQLDB);
+		script2.setDatabaseType(DatabaseType.POSTGRESQL);
+		
+		script1.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		script2.setScriptDate(parser.parseDateTime("002-2012 13:00:00"));
+		
+		assertNotSame(script1.hashCode(), script2.hashCode());
+	}
+	
+	@Test
+	public void testHashCodeIncomplete() {
+		script1.setDatabaseType(DatabaseType.HSQLDB);
+		script2.setDatabaseType(DatabaseType.POSTGRESQL);
+		
+		assertNotSame(script1.hashCode(), script2.hashCode());
+	}
+}
