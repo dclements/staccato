cdbc53fcd69c252706a3307de8092fc0c7384835
David H. Clements <dclemen@mac.com>
2012-01-25 17:37:38 -0700

    Marking the Migration annotation as inherited.
    Being done mostly for testing purposes, but has some additional benefits
    for designing more complex migration scripts.

--- src/main/java/com/readytalk/staccato/database/migration/annotation/Migration.java
+++ src/main/java/com/readytalk/staccato/database/migration/annotation/Migration.java
@@ -1,5 +1,6 @@
 package com.readytalk.staccato.database.migration.annotation;
 
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -14,6 +15,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  *
  * This is a class level annotation
  */
+@Inherited
 @Target({TYPE})
 @Retention(RUNTIME)
 public @interface Migration {
